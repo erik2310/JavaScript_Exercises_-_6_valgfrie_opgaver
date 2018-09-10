357d8b3493c3a50c1161372f2078a8b9fd46b3d2
Erik Ruhmanis <erik2310@edu.easj.dk>
2018-09-10 12:40:02 +0200

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,14 @@
+
+console.log("Reversing an array:");
+
+function reverseArray(array) {
+    let newArray = [];
+
+    for (let i = Array(array).length + 1; 0 <= i; i--) {
+        newArray.push(array[i]);
+    }
+
+    console.log(newArray);
+}
+
+reverseArray(["A", "B", "C"])
