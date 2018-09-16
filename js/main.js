7ab1d06eb589acd28f3a20bb5fbed188925fbef4
Erik Ruhmanis <erik2310@edu.easj.dk>
2018-09-16 15:42:27 +0200

    Mindre forbedringer og kommentarer.

--- js/main.js
+++ js/main.js
@@ -2,16 +2,26 @@
 console.log("Reversing an array:");
 console.log("");
 
+// En funktion til at vende om på en array
 function reverseArray(array) {
+    
+    // Deklarer en ny array som er tom
     let newArray = [];
+    
+    // Henter antallet af items i et array og minusser med 1 for at få det præcise tal og gemmer det i numberOfItemsInArray
+    let numberOfItemsInArray = array.length - 1;
 
-    for (let i = Array(array).length + 1; 0 <= i; i--) {
+    // En for loop som tæller ned fra numberOfItemsInArray
+    for (let i = numberOfItemsInArray; 0 <= i; i--) {
+        // Lægger hver array item i newArray, fra enden til starten
         newArray.push(array[i]);
     }
-
+    
+    // Udskriver newArray items
     console.log(newArray);
 }
 
+// Kalder på reverseArray funktionen
 reverseArray(["A", "B", "C"])
 
 // Mangler funktionen reverseArrayInPlace
